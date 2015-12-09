4e6e015022be59ba634ef6bd9bb10ba9fb8234d5
msnyde8 <msnyde8@students.towson.edu>
2015-12-08 19:23:29 -0800

    Updated php/html files for HW 4

---
+++ Homework/Homework4/Snyder_HW_04_01_length_get.php
@@ -0,0 +1,6 @@
+<?php
+	$result = $_GET['enterstr'];
+	$length = strlen($result);
+	echo "string length of '$result' is $length\n";
+?>
+
---
+++ Homework/Homework4/Snyder_HW_04_01_length_post.php
@@ -0,0 +1,6 @@
+<?php
+	$result = $_POST['enterstr'];
+	$length = strlen($result);
+	echo "string length of '$result' is $length\n";
+?>
+
