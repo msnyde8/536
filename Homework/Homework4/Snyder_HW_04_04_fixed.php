2118138b21361a3fe897cf61efb3941041432df5
msnyde8@students.towson.edu <msnyde8@students.towson.edu>
2015-12-09 11:36:47 -0800

    coding for HW4 #4

--- Homework/Homework4/Snyder_HW_04_04_fixed.php
+++ Homework/Homework4/Snyder_HW_04_04_fixed.php
@@ -1,5 +1,5 @@
 <?php
-	$name=$_GET['name'];
+	$name=htmlspecialchars($_GET['name'],ENT_QUOTES<'UTF-8');
 	echo "Welcome $name<br>";
 	echo "<a href=\"http://xssattackexamples.com/\">Click to Enter</a>";
 ?>
